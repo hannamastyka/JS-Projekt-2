e5c1f2f3253a741dc743a374b9a340220eeeedf2
hannamastyka <hanna.mastyka@outlook.com>
2023-09-13 19:38:27 +0200

    poprawione

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-document.getElementById("convert").addEventListener("click", function () {
+document
+  .getElementById("conventer")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
     const amount = parseFloat(document.getElementById("amount").value);
     const fromCurrency = document.getElementById("fromCurrency").value;
     const toCurrency = document.getElementById("toCurrency").value;
 
-    fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${fromCurrency}/?format=json`)
-        .then(response => response.json())
-        .then(data => {
-            const exchangeRate = data.rates[0].mid;
-            const result = amount * exchangeRate;
+    fetch(
+      `https://api.nbp.pl/api/exchangerates/rates/a/${fromCurrency}/?format=json`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data?.rates?.length > 0) {
+          const exchangeRate = data.rates[0].mid;
+          const result = amount * exchangeRate;
 
-            document.getElementById("result").innerHTML = `${amount} ${fromCurrency} to ${toCurrency}: ${result.toFixed(2)} ${toCurrency}`;
-        })
-        .catch(error => {
-            console.error(error);
-            document.getElementById("result").innerHTML = "Wystąpił błąd podczas pobierania danych z API.";
-        });
-});
+          document.getElementById("result").textContent = `${amount.toFixed(
+            2
+          )} ${fromCurrency} to ${toCurrency}: ${result.toFixed(
+            2
+          )} ${toCurrency}`;
+        }
+      })
+      .catch(() => {
+        document.getElementById("result").textContent =
+          "Wystąpił błąd podczas pobierania danych z API.";
+      });
+  });
